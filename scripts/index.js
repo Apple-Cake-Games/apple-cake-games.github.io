6a6edcf1646469f86ba4c12a5b014fc91129ee80
max-studios <244369463+max-studios@users.noreply.github.com>
2026-05-01 18:31:20 +0200

    Added link to github

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,12 @@
 const btnplay = document.getElementById("informationbtn")
+const btnGit = document.getElementById("icnGithub")
 
 btnplay.addEventListener("click", function(){
 
     window.open("/html/alienhazard.html");
 
+});
+
+btnGit.addEventListener("click", function(){
+    window.open("https://github.com/Apple-Cake-Games/apple-cake-games.github.io");
 })
