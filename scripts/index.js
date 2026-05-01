85823846ee44fd060976813c6f8025cb7db1da54
max-studios <244369463+max-studios@users.noreply.github.com>
2026-05-01 13:15:02 +0200

    Added Game

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,6 @@ const btnplay = document.getElementById("informationbtn")
 
 btnplay.addEventListener("click", function(){
 
-    window.open("https://max-studios.github.io/");
+    window.open("/html/game.html");
 
 })
