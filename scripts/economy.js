caa9cf949fb1ea86195fcd96078ac1ab0d3c7edf
max-studios <244369463+max-studios@users.noreply.github.com>
2026-05-02 14:11:33 +0200

    Updated Skins

--- scripts/economy.js
+++ scripts/economy.js
@@ -5,27 +5,37 @@ const SKINS = {
   default: {
     name: 'Green Alien',
     price: 0,
-    img: '../assets/gameelements/player.png',
+    img: '../assets/gameelements/player.gif',
   },
   '7012734ß09172349ß075231907ß10237509ß1235': {
     name: 'Red Alien',
     price: 50,
-    img: '../assets/gameelements/skins/red.png',
+    img: '../assets/gameelements/skins/red.gif',
   },
   '4376891235609812368586089216358902163553': {
     name: 'Pink Alien',
     price: 100,
-    img: '../assets/gameelements/skins/pink.png',
+    img: '../assets/gameelements/skins/pink.gif',
   },
   '7853201750213568126539062130981236508213': {
-    name: 'Void UFO',
+    name: 'Void',
     price: 200,
-    img: '../assets/gameelements/skins/void.png',
+    img: '../assets/gameelements/skins/void.gif',
   },
   '7012865092165309812658906523906529106210': {
-    name: 'Gold UFO',
+    name: 'Gold',
     price: 500,
-    img: '../assets/gameelements/skins/gold.png',
+    img: '../assets/gameelements/skins/gold.gif',
+  },
+  '3521532563242314321543265145315446431135': {
+    name: 'Robot',
+    price: 700,
+    img: '../assets/gameelements/skins/robot.gif',
+  },
+  '78032790127903678956063210965213902536982': {
+    name: 'Warship',
+    price: 1000,
+    img: '../assets/gameelements/skins/warship.gif',
   },
   '32479023479023479023ß12370ß92134709ß2144': {
     name: 'AppleCakeGameSpecial',
